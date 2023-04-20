b2d4a36fb469d76d5571e957de855974ed4f9c40
anilsirige <anilsirige123@gmail.com>
2023-04-20 11:12:12 +0530

    Student class

--- src/StudentFile.java
+++
@@ -1,8 +0,0 @@
-/**
- * @Author ➤➤➤ Anil
- * @Date ➤➤➤ 20/04/23
- * @Time ➤➤➤ 11:05 am
- * @Project ➤➤➤ For git
- */
-public class StudentFile {
-}
---
+++ src/StudentFile1.java
@@ -0,0 +1,11 @@
+/**
+ * @Author ➤➤➤ Anil
+ * @Date ➤➤➤ 20/04/23
+ * @Time ➤➤➤ 11:05 am
+ * @Project ➤➤➤ For git
+ */
+public class StudentFile1 {
+    public static void main(String[] args) {
+        System.out.println("hi tried new");
+    }
+}
